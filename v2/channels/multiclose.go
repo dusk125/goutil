13926177e197f463f6013ca8e55530e6b79875ab
Allen Ray <allentray125@gmail.com>
2022-05-26 16:46:59 -0400

    Update MulticloseChan documentation

--- v2/channels/multiclose.go
+++ v2/channels/multiclose.go
@@ -2,39 +2,47 @@ package channels
 
 import "sync"
 
+// ChanWriter gives permission to a callee to write to a channel
 type ChanWriter[T any] interface {
 	Write(v T)
 }
 
+// ChanReader gives permission to a callee to read from a channel
 type ChanReader[T any] interface {
 	Read() (v T, ok bool)
 }
 
+// ChanCloser gives permission to a callee to close a channel
 type ChanCloser[T any] interface {
 	Close()
 }
 
+// Gives both read and close permissions
 type ChanReadCloser[T any] interface {
 	ChanReader[T]
 	ChanCloser[T]
 }
 
+// Gives both write and close permissions
 type ChanWriteCloser[T any] interface {
 	ChanWriter[T]
 	ChanCloser[T]
 }
 
+// Gives both read and write permissions
 type ChanReadWriter[T any] interface {
 	ChanReader[T]
 	ChanWriter[T]
 }
 
+// Gives all read, write, and close permissions
 type ChanReadWriterCloser[T any] interface {
 	ChanReader[T]
 	ChanWriter[T]
 	ChanCloser[T]
 }
 
+// The Multiclose Chan is a channel that can only ever be closed once, additional calls to Close() will be a no-op.
 type MulticloseChan[T any] struct {
 	ch chan T
 	o  sync.Once
@@ -46,14 +54,19 @@ func MulticloseMake[T any](buffered int) *MulticloseChan[T] {
 	}
 }
 
+// Close the MulticloseChan; multiple invocations (across goroutines) are allowed.
+// 	If this is the first time this has been called on this channel, close it;
+// 	if this is a subsequent call to close, then no-op.
 func (m *MulticloseChan[T]) Close() {
 	m.o.Do(func() { close(m.ch) })
 }
 
+// Write to the underlying channel
 func (m *MulticloseChan[T]) Write(v T) {
 	m.ch <- v
 }
 
+// Read from the underlying channel
 func (m *MulticloseChan[T]) Read() (v T, ok bool) {
 	v, ok = <-m.ch
 	return
